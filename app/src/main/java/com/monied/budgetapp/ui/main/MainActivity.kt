eca21aa57083f7024e0208c0442ed7818913c81f
@KaraboMakoro <st10461176@rcconnect.edu.za>
2026-04-28 15:07:28 +0200

    Updated Main Activity

--- app/src/main/java/com/monied/budgetapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/monied/budgetapp/ui/main/MainActivity.kt
@@ -57,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
             .commit()
-        
+
     }
 
     fun openProfile() {
