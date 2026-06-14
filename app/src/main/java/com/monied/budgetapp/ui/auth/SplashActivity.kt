0ab9a6f9c5181f5c733bcae2afd7be81fc96b2d2
Vvukeya <ST10445789@rcconnect.edu.za>
2026-06-14 20:03:20 +0200

    Suppress custom splash screen lint warning in SplashActivity
    - Added `@SuppressLint("CustomSplashScreen")` to `SplashActivity`.
    - Added necessary import for `android.annotation.SuppressLint`.

--- app/src/main/java/com/monied/budgetapp/ui/auth/SplashActivity.kt
+++ app/src/main/java/com/monied/budgetapp/ui/auth/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.monied.budgetapp.ui.auth
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -7,6 +8,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.monied.budgetapp.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
